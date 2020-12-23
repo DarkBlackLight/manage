5c207ac863916999e0232d14119a21ed7577eb5e
liwuqi95 <wuqi.li@mail.utoronto.ca>
2020-12-23 19:09:38 +0800

    adding concerns

---
+++ app/concerns/manage/controllers/manage_controller.rb
@@ -0,0 +1,20 @@
+module Manage::Concerns::Controllers::ManageController
+  extend ActiveSupport::Concern
+
+  layout 'manage/application'
+
+  before_action :authenticate_user!
+  before_action :setup_routes
+
+  private
+
+  def setup_routes
+    @routes = [{
+                 name: "Dashboard",
+                 url: url_for({ controller: :dashboard, action: :index }),
+                 icon: "fas fa-tachometer-alt",
+                 can: (can? :index, :dashboard)
+               }]
+  end
+
+end
--- app/controllers/manage/manage_controller.rb
+++ app/controllers/manage/manage_controller.rb
@@ -1,20 +1,5 @@
 module Manage
   class ManageController < ApplicationController
-    layout 'manage/application'
-
-    before_action :authenticate_user!
-    before_action :setup_routes
-
-    private
-
-    def setup_routes
-      @routes = [{
-                   name: "Dashboard",
-                   url: url_for({ controller: :dashboard, action: :index }),
-                   icon: "fas fa-tachometer-alt",
-                   can: (can? :index, :dashboard)
-                 }]
-    end
-
+    include Manage::Concerns::Controllers::ManageController
   end
 end
