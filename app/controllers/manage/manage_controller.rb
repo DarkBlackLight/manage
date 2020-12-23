14ce3b463315885c04894037dc61db4349965781
liwuqi95 <wuqi.li@mail.utoronto.ca>
2020-12-23 19:44:39 +0800

    minor fix

--- app/controllers/manage/manage_controller.rb
+++ app/controllers/manage/manage_controller.rb
@@ -1,5 +1,5 @@
 module Manage
   class ManageController < ApplicationController
-    include Manage::Concerns::Controllers::ManageController
+    include Manage::Concerns::Controllers::Article
   end
 end
--- lib/concerns/controllers/manage_controller.rb
+++ lib/concerns/controllers/manage_controller.rb
@@ -1,5 +1,3 @@
-require 'active_support/concern'
-
 module Manage::Concerns::Controllers::ManageController
   extend ActiveSupport::Concern
 
