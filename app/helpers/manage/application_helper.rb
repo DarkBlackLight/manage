b3d4254b79e9905ca5c6a050b8e155048b4c377f
liwuqi95 <wuqi.li@mail.utoronto.ca>
2020-12-23 16:05:17 +0800

    minor fix

--- app/helpers/manage/application_helper.rb
+++ app/helpers/manage/application_helper.rb
@@ -1,56 +1,44 @@
 module Manage
   module ApplicationHelper
-    def manage_text_field(form, field, options1 = {}, options2 = { class: 'col-md-4' })
-      options1[:class] = "form-control" unless options1[:class]
-      tag.div options2 do
-        tag.div class: 'form-group' do
-          (form.label field) + (form.text_field field, options1)
-        end
+    def manage_text_field(form, field, options = {})
+      options[:class] = "#{options[:class]} form-control"
+      tag.div class: 'form-group' do
+        (form.label field) + (form.text_field field, options)
       end
     end
 
-    def manage_number_field(form, field, options1 = {}, options2 = { class: 'col-md-4' })
-      options1[:class] = "form-control" unless options1[:class]
-      tag.div options2 do
-        tag.div class: 'form-group' do
-          (form.label field) + (form.number_field field, options1)
-        end
+    def manage_number_field(form, field, options = {})
+      options[:class] = "#{options[:class]} form-control"
+      tag.div class: 'form-group' do
+        (form.label field) + (form.number_field field, options)
       end
     end
 
-    def manage_text_area_field(form, field, options1 = {}, options2 = { class: 'col-md-4' })
-      options1[:class] = "form-control" unless options1[:class]
-      tag.div options2 do
-        tag.div class: 'form-group' do
-          (form.label field) + (form.text_area field, options1)
-        end
+    def manage_text_area_field(form, field, options = {})
+      options[:class] = "#{options[:class]} form-control"
+      tag.div class: 'form-group' do
+        (form.label field) + (form.text_area field, options)
       end
     end
 
-    def manage_collection_select_field(form, field, collection, value_method, text_method, options1 = {}, options2 = {}, options3 = { class: 'col-md-4' })
-      options2[:class] = "form-control select2" unless options2[:class]
-      tag.div options3 do
-        tag.div class: 'form-group' do
-          (form.label field) + (form.collection_select field, collection, value_method, text_method, options1, options2)
-        end
+    def manage_collection_select_field(form, field, collection, value_method, text_method, options = {}, html_options = {})
+      html_options[:class] = "#{html_options[:class]} form-control"
+      tag.div class: 'form-group' do
+        (form.label field) + (form.collection_select field, collection, value_method, text_method, options, html_options)
       end
     end
 
-    def manage_select_field(form, field, choices, options1 = {}, options2 = {}, options3 = { class: 'col-md-4' })
-      options2[:class] = "form-control select2" unless options2[:class]
-      tag.div options3 do
-        tag.div class: 'form-group' do
-          (form.label field) + (form.select field, choices, options1, options2)
-        end
+    def manage_select_field(form, field, choices, options = {}, html_options = {})
+      html_options[:class] = "#{html_options[:class]} form-control"
+      tag.div class: 'form-group' do
+        (form.label field) + (form.select field, choices, options, html_options)
       end
     end
 
-    def manage_text_tag(label, name, value, options1 = {}, options2 = { class: 'col-md-4' })
-      options1[:class] = "form-control" unless options1[:class]
-      tag.div options2 do
-        tag.div class: 'form-group' do
-          (label_tag label) + (text_field_tag name, value, options1)
-        end
+    def manage_text_tag(label, name, value, options = {})
+      options[:class] = "form-control" unless options[:class]
+      tag.div class: 'form-group' do
+        (label_tag label) + (text_field_tag name, value, options)
       end
     end
   end
