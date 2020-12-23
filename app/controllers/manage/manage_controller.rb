d10282140cd9020dff93e5b806d8622741575202
liwuqi95 <wuqi.li@mail.utoronto.ca>
2020-12-23 16:30:35 +0800

    minor fix

--- app/controllers/manage/manage_controller.rb
+++ app/controllers/manage/manage_controller.rb
@@ -4,5 +4,34 @@ module Manage
 
     before_action :authenticate_user!
 
+    private
+
+    def routes
+      [{
+         name: "Dashboard",
+         url: url_for({ controller: :dashboard, action: :index }),
+         icon: "cil-calendar",
+       }]
+    end
+
+    def render_sidebar
+      routes.map do |route|
+
+        route[:children] ?
+
+          ()
+          :
+          (
+            tag.li class: "c-sidebar-nav-item" do
+              link_to routes[:url], class: "c-sidebar-nav-link" do
+                tag.svg class: "c-sidebar-nav-icon" do
+                  tag.use "xlink:href": "/manage/svg/free.svg##{route[:icon]}"
+                end
+              end
+            end
+          )
+      end
+    end
+
   end
 end
