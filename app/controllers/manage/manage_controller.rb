d74bf7a3e9ed07752f2963ec7d016f9071b18482
liwuqi95 <wuqi.li@mail.utoronto.ca>
2020-12-23 14:29:59 +0800

    minor fix

--- app/controllers/manage/manage_controller.rb
+++ app/controllers/manage/manage_controller.rb
@@ -1,4 +1,9 @@
 module Manage
   class ManageController < ActionController::Base
+    before_action :authenticate_manage_user!
+
+    def current_ability
+      @current_ability ||= Ability.new(current_manage_user)
+    end
   end
 end
