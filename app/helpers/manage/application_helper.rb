22697090687117ff0a9aed29c844dacdfbf7cd01
liwuqi95 <wuqi.li@mail.utoronto.ca>
2020-12-24 16:09:01 +0800

    minor fix

--- app/helpers/manage/application_helper.rb
+++ app/helpers/manage/application_helper.rb
@@ -49,21 +49,21 @@ module Manage
     end
 
     def manage_collection_select_field(form, field, collection, value_method, text_method, options = {}, html_options = {})
-      html_options[:class] = "#{html_options[:class]} form-control"
+      html_options[:class] = "#{html_options[:class]} form-control select2"
       tag.div class: 'form-group' do
         (form.label field) + (form.collection_select field, collection, value_method, text_method, options, html_options)
       end
     end
 
     def manage_select_field(form, field, choices, options = {}, html_options = {})
-      html_options[:class] = "#{html_options[:class]} form-control"
+      html_options[:class] = "#{html_options[:class]} form-control select2"
       tag.div class: 'form-group' do
         (form.label field) + (form.select field, choices, options, html_options)
       end
     end
 
     def manage_text_tag(label, name, value, options = {})
-      options[:class] = "form-control" unless options[:class]
+      options[:class] = "#{options[:class]} form-control"
       tag.div class: 'form-group' do
         (label_tag label) + (text_field_tag name, value, options)
       end
