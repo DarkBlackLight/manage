fbe89f6874c7c065e195ba17a8eb651b0371971f
liwuqi95 <wuqi.li@mail.utoronto.ca>
2020-12-24 09:31:44 +0800

    minor fix

--- app/controllers/concerns/manage/concern_manage_controller.rb
+++ app/controllers/concerns/manage/concern_manage_controller.rb
@@ -2,20 +2,24 @@ module Manage
   module ConcernManageController
     extend ActiveSupport::Concern
 
-    layout 'manage/application'
+    included do
 
-    before_action :authenticate_user!
-    before_action :setup_routes
+      layout 'manage/application'
 
-    private
+      before_action :authenticate_user!
+      before_action :setup_routes
 
-    def setup_routes
-      @routes = [{
-                   name: "Dashboard",
-                   url: url_for({ controller: :dashboard, action: :index }),
-                   icon: "fas fa-tachometer-alt",
-                   can: (can? :index, :dashboard)
-                 }]
+      private
+
+      def setup_routes
+        @routes = [{
+                     name: "Dashboard",
+                     url: url_for({ controller: :dashboard, action: :index }),
+                     icon: "fas fa-tachometer-alt",
+                     can: (can? :index, :dashboard)
+                   }]
+      end
+
     end
 
   end
