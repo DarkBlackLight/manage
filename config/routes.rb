0880f6ce8c2253d8d627e6ff9dfff20b835baaec
liwuqi95 <wuqi.li@mail.utoronto.ca>
2020-12-24 14:00:32 +0800

    minor fix

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Manage::Engine.routes.draw do
-  devise_for :users
+  devise_for :users, controllers: { confirmations: 'confirmations' }
   get 'dashboard/index', to: 'dashboard#index'
   root 'dashboard#index'
 end
