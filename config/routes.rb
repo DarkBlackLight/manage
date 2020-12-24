6b684036c2609a0b1d5b18d97f81c6f9e3994c8f
liwuqi95 <wuqi.li@mail.utoronto.ca>
2020-12-24 19:18:28 +0800

    minor fix

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,8 @@
 Manage::Engine.routes.draw do
-  devise_for :users, {
-    class_name: 'User',
-    module: :devise
+  devise_for :users, :controllers => {
+    :sessions => "manage/sessions"
   }
-  
+
   get 'dashboard/index', to: 'dashboard#index'
   root 'dashboard#index'
 end
