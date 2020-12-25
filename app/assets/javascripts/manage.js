e1ef5a79562534d58dcff71313383cdbae6600f7
liwuqi95 <wuqi.li@mail.utoronto.ca>
2020-12-25 22:19:58 +0800

    minor fix

--- app/assets/javascripts/manage.js
+++ app/assets/javascripts/manage.js
@@ -1,15 +1,16 @@
-//= require rails-ujs
 //= require coreui/coreui.bundle.min.js
 //= require coreui/svgxuse.min
 //= require coreui/coreui-chartjs.bundle
 //= require coreui/coreui-utils
 //= require coreui/jquery.min
+//= require rails-ujs
 //= require coreui/jquery-ui.min
 //= require coreui/toastr
 //= require coreui/moment.min
 //= require coreui/select2.min
 //= require coreui/daterangepicker
 //= require coreui/full-calendar
+
 //= require customize_manage
 //= require cocoon
 //= require rails_sortable
