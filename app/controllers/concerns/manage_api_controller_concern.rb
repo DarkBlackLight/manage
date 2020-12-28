6e7f77877c6cbb719f0480ff69438479331091c5
liwuqi95 <wuqi.li@mail.utoronto.ca>
2020-12-28 18:35:26 +0800

    minor fix

--- app/controllers/concerns/manage_api_controller_concern.rb
+++ app/controllers/concerns/manage_api_controller_concern.rb
@@ -1,6 +1,13 @@
 module ManageControllerConcern
   extend ActiveSupport::Concern
   included do
+    before_action :setup_user
 
+    def setup_user
+      if request.headers['Authorization'] && (request.headers['Authorization'].include? 'Token')
+        @current_user = User.find_by_token(request.headers['Authorization'].split(' ')[1])
+        @current_user = nil unless @current_instructor && @current_instructor.token
+      end
+    end
   end
 end
