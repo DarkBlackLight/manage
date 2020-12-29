b1c2f850759924238f5903f32db4e2c13111f25b
liwuqi95 <wuqi.li@mail.utoronto.ca>
2020-12-29 10:14:12 +0800

    minor fix

--- app/controllers/manage/sessions_controller.rb
+++ app/controllers/manage/sessions_controller.rb
@@ -5,9 +5,10 @@ class Manage::SessionsController < Devise::SessionsController
   # before_action :configure_sign_in_params, only: [:create]
 
   # GET /resource/sign_in
-  # def new
-  #   super
-  # end
+  def new
+    @authentication_key = 'email'
+    super
+  end
 
   # POST /resource/sign_in
   # def create
