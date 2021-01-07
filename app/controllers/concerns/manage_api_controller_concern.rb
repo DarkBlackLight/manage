82737a0f60dacbb2f6730c9e720a76a6c31c4ba3
liwuqi95 <wuqi.li@mail.utoronto.ca>
2021-01-07 18:20:22 +0800

    minor fix

--- app/controllers/concerns/manage_api_controller_concern.rb
+++ app/controllers/concerns/manage_api_controller_concern.rb
@@ -4,9 +4,9 @@ module ManageApiControllerConcern
     before_action :setup_user
 
     def setup_user
-      if request.headers['Authorization'] && (request.headers['Authorization'].include? 'Token')
+      if request.headers['Authorization'] && (request.headers['Authorization'].include? 'Bearer')
         @current_user = User.find_by_token(request.headers['Authorization'].split(' ')[1])
-        @current_user = nil unless @current_instructor && @current_instructor.token
+        @current_user = nil unless @current_user && @current_user.token
       end
     end
 
