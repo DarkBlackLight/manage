939717330e8c791aa53ff72e367d0e8b9b16cccf
liwuqi95 <wuqi.li@mail.utoronto.ca>
2021-01-07 19:46:30 +0800

    minor fix

--- app/controllers/concerns/manage_api_controller_concern.rb
+++ app/controllers/concerns/manage_api_controller_concern.rb
@@ -1,7 +1,7 @@
 module ManageApiControllerConcern
   extend ActiveSupport::Concern
   included do
-    skip_before_filter :verify_authenticity_token
+    skip_before_action :verify_authenticity_token
     before_action :setup_user
 
     def setup_user
