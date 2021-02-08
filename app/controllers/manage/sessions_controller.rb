2820b65adc0b90d8cfa3345a2903c4892ac97259
liwuqi95 <wuqi.li@mail.utoronto.ca>
2021-02-08 10:56:06 +0800

    minor fix

--- app/controllers/manage/sessions_controller.rb
+++ app/controllers/manage/sessions_controller.rb
@@ -5,4 +5,8 @@ class Manage::SessionsController < Devise::SessionsController
   def setup_manage_config
     @config = setup_config
   end
+
+  def after_sign_out_path_for(resource_or_scope)
+    request.referrer
+  end
 end
