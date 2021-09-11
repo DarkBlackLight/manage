5a91f5f96b8d35298893b810cde03d9e08d4c5ec
liwuqi95 <wuqi.li@mail.utoronto.ca>
2021-09-10 21:26:44 -0400

    minor fix

--- app/models/user_view.rb
+++ app/models/user_view.rb
@@ -1,3 +1,6 @@
 class UserView < ApplicationRecord
+  scope :query_date_from, ->(q) { where('user_views.created_at >= ?', Time.zone.parse(q)) }
+  scope :query_date_to, ->(q) { where('user_views.created_at <= ?', Time.zone.parse(q)) }
+
   belongs_to :user, optional: true
 end
