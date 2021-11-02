74c194f3bd7f3a009de12d56b678e2c70faa02f1
liwuqi95 <wuqi.li@mail.utoronto.ca>
2021-11-02 10:05:42 -0400

    minor fix

--- app/controllers/concerns/manage_controller_concern.rb
+++ app/controllers/concerns/manage_controller_concern.rb
@@ -7,7 +7,6 @@ module ManageControllerConcern
     before_action :setup_manage_config
     before_action :setup_locale
     before_action :setup_routes
-    before_action :setup_user_view
 
     private
 
@@ -38,14 +37,14 @@ module ManageControllerConcern
     end
 
     def setup_user_view
-      # if @config[:user_view_disabled] == nil || @config[:user_view_disabled] == false
-      #   user = eval("current_#{@config[:scope]}_user")
-      #   UserView.create!(user: user,
-      #                    view_controller: controller_name,
-      #                    view_action: action_name,
-      #                    view_params: params.to_s,
-      #                    ip_address: request.remote_ip)
-      # end
+      if @config[:user_view_disabled] == nil || @config[:user_view_disabled] == false
+        user = eval("current_#{@config[:scope]}_user")
+        UserView.create!(user: user,
+                         view_controller: controller_name,
+                         view_action: action_name,
+                         view_params: params.to_s,
+                         ip_address: request.remote_ip)
+      end
     end
 
   end
